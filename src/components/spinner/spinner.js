c87d7ab3619d349e0e9511856393489a6efa8754
noiseLim <noiselim@gmail.com>
2020-12-06 19:44:40 +0300

    add spinner and script 'catch error'

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-rolling-zhnljvywc"><div class="ldio-xc41ddp9usf">
+        <div></div>
+        </div></div>
+    )
+}
+
+export default Spinner;
